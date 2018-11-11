151a959f75555779cc68980347a8476f7a2bb338
Kw Huang <kuanweih@andrew.cmu.edu>
2018-11-10 22:50:41 -0500

    done sed

--- kde.sh
+++ kde.sh
@@ -2,15 +2,16 @@
 
 
 # kde_path="/home/kwhuang/bash-multi-pbs"
-job_path="/home/kwhuang/bash-multi-pbs/Fornax"
-source_path="/home/kwhuang/bash-multi-pbs/KDE-2-Gaussian"
+job_path="/Users/kwhuang/bash-multi-pbs/Fornax"
+source_path="/Users/kwhuang/bash-multi-pbs/KDE-2-Gaussian"
 
 
 kernels='gaussian  poisson'
 s_ones='0.005  0.01'
 s_twos='0.01  0.05  0.1'
-ngs='400  600  800'
-r_ares='1.0  2.0  3.0  4.0  5.0  6.0  7.0  8.0  9.0  10.0'
+ngs='400  600'
+r_areas='1.0  2.0'
+# r_areas='1.0  2.0  3.0  4.0  5.0  6.0  7.0  8.0  9.0  10.0'
 
 
 
@@ -18,23 +19,19 @@ for kernel in $kernels;  do
 for s_one in $s_ones;  do
 for s_two in $s_twos;  do
 for ng in $ngs;  do
-for r_area in $r_area;  do
+for r_area in $r_areas;  do
 
-  run_name="$kernel"_"$ng"_"$s_one"_"$s_two"_"$r_area"
+  run_name=$kernel"_N"$ng"_s"$s_one"_s"$s_two"_r"$r_area
   cd  $job_path
-  mkdir $run_name
-  # cd  $job_path
-  # mkdir "hahas"
+  cp  -r  $source_path  $run_name
+  cd  $run_name
 
 
-  # cp  -r  "$source_path"  "$run_name"
-  # cd  "$run_name"
-  #
-  # sed -i "s/^NUM_GRID =*/NUM_GRID = ${ng}    # number of meshgrids/"  param_den.py
-  # sed -i "s/^SIGMA1 =*/SIGMA1 = ${s_one}    # searching scale in deg/"  param_den.py
-  # sed -i "s/^SIGMA2 =*/SIGMA2 = ${s_two}    # background scale in deg/"  param_den.py
-  # sed -i "s/^KERNEL_BG = */KERNEL_BG = ${kernel}    # kernel/"  param_den.py
-  # sed -i "s/^RATIO_AREA_TG_BG =*/RATIO_AREA_TG_BG = ${r_area}    # ratio of area/"  param_den.py
+  sed  "s/NUM_GRID = .*/NUM_GRID = $ng    # number of meshgrids/g"  param_den.py > tmp.py  &&  mv  tmp.py  param_den.py
+  sed  "s/SIGMA1 = .*/SIGMA1 = $s_one    # searching scale in deg/g"  param_den.py > tmp.py  &&  mv  tmp.py  param_den.py
+  sed  "s/SIGMA2 = .*/SIGMA2 = $s_two    # background scale in deg/g"  param_den.py > tmp.py  &&  mv  tmp.py  param_den.py
+  sed  "s/KERNEL_BG = .*/KERNEL_BG = '$kernel'    # kernel/g"  param_den.py > tmp.py  &&  mv  tmp.py  param_den.py
+  sed  "s/RATIO_AREA_TG_BG = .*/RATIO_AREA_TG_BG = $r_area    # ratio of area/g"  param_den.py > tmp.py  &&  mv  tmp.py  param_den.py
 done  #  for r_area
 done  #  for ng
 done  #  for s_two
