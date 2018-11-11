6bc641f90069b44940976cbef3b43f4a350ff844
Kw Huang <kuanweih@andrew.cmu.edu>
2018-11-11 14:03:04 -0500

    done!

--- kde.sh
+++ kde.sh
@@ -1,18 +1,17 @@
 #!/bin/bash
 
 
-# kde_path="/home/kwhuang/bash-multi-pbs"
-job_path="/home/kwhuang/bash-multi-pbs/Fornax"
-source_path="/home/kwhuang/bash-multi-pbs/KDE-2-Gaussian"
+job_path="/Users/kwhuang/bash-multi-pbs/Fornax"
+source_path="/Users/kwhuang/bash-multi-pbs/KDE-2-Gaussian"
 
 
 radiuss='2.0'
 pmnans='True  False'
-pmcuts='1.0'  # std of pm
+pmcuts='0  1'  # std of pm
 
 kernels='gaussian  poisson'
 s_ones='0.005  0.01'
-s_twos='0.01  0.05  0.1'
+s_twos='0.05  0.1'
 ngs='400  600'
 r_areas='1.0  2.0'
 # r_areas='1.0  2.0  3.0  4.0  5.0  6.0  7.0  8.0  9.0  10.0'
@@ -25,13 +24,17 @@ for ng in $ngs;  do
 for r_area in $r_areas;  do
 for radius in $radiuss;  do
 for pmnan in $pmnans;  do
-for pmnan in $pmnans;  do
+for pmcut in $pmcuts;  do
 
-  if [ $pmnan = "True" ]; then
+  if [ $pmnan = "True" ] && [ $pmcut != "0" ]; then
+    run_name="R"$radius"_"$kernel"_N"$ng"_s"$s_one"_s"$s_two"_r"$r_area"_pmcut_"$pmcut
+  elif [ $pmnan = "True" ] && [ $pmcut = "0" ]; then
     run_name="R"$radius"_"$kernel"_N"$ng"_s"$s_one"_s"$s_two"_r"$r_area"_pmnan"
-  else
+  elif [ $pmnan = "False" ] && [ $pmcut = "0" ]; then
     run_name="R"$radius"_"$kernel"_N"$ng"_s"$s_one"_s"$s_two"_r"$r_area
-  fi  # if pmnan
+  else
+    break;
+  fi
 
   cd  $job_path
   cp  -r  $source_path  $run_name
@@ -47,6 +50,17 @@ for pmnan in $pmnans;  do
   sed  "s/REMOVE_PM_NAN = .*/REMOVE_PM_NAN = $pmnan    # True:on, Flase:off/g"  param_get.py > tmp.py  &&  mv  tmp.py  param_get.py
 
 
+  if [ $pmcut != "0" ]; then
+    sed  "s/PM_CUT = .*/PM_CUT = True    # True:on, Flase:off/g"  param_get.py > tmp.py  &&  mv  tmp.py  param_get.py
+  else
+    sed  "s/PM_CUT = .*/PM_CUT = False    # True:on, Flase:off/g"  param_get.py > tmp.py  &&  mv  tmp.py  param_get.py
+    sed  "s/    PM_CUT_STD = .*/P    PM_CUT_STD = $pmcut/g"  param_get.py > tmp.py  &&  mv  tmp.py  param_get.py
+  fi
+
+  qsub  submit-coma.job  -q  bigmem
+
+
+done  #  for pmcut
 done  #  for pmnan
 done  #  for radius
 done  #  for r_area
@@ -54,13 +68,3 @@ done  #  for ng
 done  #  for s_two
 done  #  for s_one
 done  #  for kernel
-
-
-        #
-        # cp -r /home/yhe2/PM_Pair_mlt/Fix_Power/Pair2/Pair2_temp /home/yhe2/PM_Pair_mlt/Fix_Power/Pair2/Pair2_${j}-${k}
-        #
-        # cd /home/yhe2/PM_Pair_mlt/Fix_Power/Pair2/Pair2_${j}-${k}
-        # sed -i "2s/PM_Pair2/PM_Pair2_${j}-${k}/g" jobs.pbs
-        # sed -i "20s/k=0/k=${l}/g" jobs.pbs
-        # qsub jobs.pbs
-# done
